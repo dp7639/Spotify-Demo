297a8980da1b2bade7b25bef0cb4679c7404d393
dp7639 <dp736116@gmail.com>
2022-08-02 15:10:43 +0530

    build

---
+++ components/Sidebar.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import {HomeIcon,SearchIcon,LibraryIcon,PlusCircleIcon, HeartIcon, RssIcon} from "@heroicons/react/outline"
+import {signOut, useSession} from "next-auth/react"
+import useSpotify from '../hooks/useSpotify'
+import spotifyApi from '../lib/spotify'
+
+
+
+function Sidebar() {
+  const spotifyApi=useSpotify()
+  const {data: session,status}=useSession()
+  const[playlists,setplaylists]=useState([])
+  
+  useEffect(()=> {
+    if(spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists()
+      .then((data)=> {
+        setplaylists(data.body.items)
+      });
+    } 
+    
+  },[session,spotifyApi])
+
+  return (
+    <div className='text-gray-500 p-5 text-sm
+    border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
+      <div className='space-y-4'>
+        <button className='flex items-center space-x-2
+        hover:text-white' onClick={()=>signOut()}>
+          <HomeIcon className='h-5 w-5' />
+          <p>Log out</p>
+        </button>
+        <button className='flex items-center space-x-2
+        hover:text-white'>
+          <SearchIcon className='h-5 w-5' />
+          <p>Search</p>
+        </button>
+        <button className='flex items-center space-x-2
+        hover:text-white'>
+          <LibraryIcon className='h-5 w-5' />
+          <p>Your Library</p>
+        </button>
+        <hr className='border-t-[0.1px] border-gray-900'/>
+
+        <button className='flex items-center space-x-2
+        hover:text-white'>
+          <PlusCircleIcon className='h-5 w-5' />
+          <p>Create Playlist</p>
+        </button>
+        <button className='flex items-center space-x-2
+        hover:text-white'>
+          <HeartIcon className='h-5 w-5' />
+          <p>Liked Songs</p>
+        </button>
+        <button className='flex items-center space-x-2
+        hover:text-white'>
+          <RssIcon className='h-5 w-5' />
+          <p>Your episodes</p>
+        </button>
+        
+        <hr className='border-t-[0.1px] border-gray-900'/>
+        {/* Playlists... */}
+        {playlists.map((playlist)=> (
+         <p key={playlist.id} className='cursor-pointer hover:text-white'>{playlist.name}</p>
+        ))}
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
